3a69f10165841b987d05734ea36de5f6385b8e3b
Huzaifa Shaikh <huzaifashaikh09h@gmail.com>
2025-09-03 18:02:59 +0530

    typeof change

--- test.js
+++ test.js
@@ -3,3 +3,18 @@ let score = 33
 let stringNumber = String(score)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+const id = Symbol('123') 
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+const heros = ["aam","jam","kam"]
+let obj = 
+{
+    name:"neecj",
+    age: 22,
+}
+
+const myfunction= function (){
+    console.log("hellow ");
+}
+
+console.log(typeof obj);
